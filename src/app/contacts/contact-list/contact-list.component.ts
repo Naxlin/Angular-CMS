5bd05a2c18e42573762efbcdff476b28548b49bd
naxlin <naxlin.everthaine@gmail.com>
2020-09-24 15:38:39 -0600

    CMS setup

---
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
