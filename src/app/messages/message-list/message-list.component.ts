ef6fa290a157f7f051fcd741d9dea04dbfb8d618
naxlin <naxlin.everthaine@gmail.com>
2020-10-01 16:23:44 -0600

    View Switching completed

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  messages: Message[] = [
+    new Message(1, "subject 1", "this is message 1", "Jonathan Smith"),
+    new Message(2, "subject 2", "this is message 2", "Jonathan Smith"),
+    new Message(3, "subject 3", "this is message 3", "Jonathan Smith"),
+  ];
 
   constructor() { }
 
