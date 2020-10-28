6dff11ce55b25d026123c5c62360cea5238ccadb
naxlin <naxlin.everthaine@gmail.com>
2020-10-28 12:23:04 -0600

    Finished setting up Add, Update, & Delete for docs

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-edit',
--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -9,14 +9,14 @@ import { MOCKDOCUMENTS } from "./MOCKDOCUMENTS";
 export class DocumentService {
   // List of documents for the whole application
   private documents: Document[] = [];
+  private maxDocId: number;
   // Setting up event emitters
-  documentSelectedEvent = new Subject<Document>();
   documentListChangedEvent = new Subject<Document[]>();
-
 
   // Imports from constant list of documents
   constructor() { 
     this.documents = MOCKDOCUMENTS;
+    this.maxDocId = this.getMaxId();
   }
 
   // Returns a copy of all documents
@@ -29,21 +29,64 @@ export class DocumentService {
     return this.documents.find(doc => doc.id === id);
   }
 
+  // Returns the largest ID in the array
+  getMaxId() {
+    let maxId = 0;
+    this.documents.map(doc => { if (maxId < +doc.id) { maxId = +doc.id }});
+    console.log(maxId);
+    return maxId;
+  }
+
+  // Adds a new document with a new unique ID to the documents array
+  addDocument(newDoc: Document) {
+    // Ensuring a document exists
+    if (!newDoc)
+      return;
+
+    // Creating and setting unique ID based on previous maxDocId
+    this.maxDocId++;
+    newDoc.id = this.maxDocId.toString();
+
+    // Pushing the new doc onto the doc list
+    this.documents.push(newDoc);
+    
+    // Emitting change (reusing getDocuments() to reuse sort)
+    this.documentListChangedEvent.next(this.getDocuments());
+  }
+
+  // Updates a document with a new one, replacing the old doc obj
+  updateDocument(ogDoc: Document, newDoc: Document) {
+    // Ensuring both docs exists
+    if (!ogDoc || !newDoc)
+      return;
+
+    // If invalid index: leave function
+    const pos = this.documents.indexOf(ogDoc);
+    if (pos < 0) 
+      return;
+
+    // Replacing original doc with new one
+    newDoc.id = ogDoc.id;
+    this.documents[pos] = newDoc;
+
+    // Emitting change (reusing getDocuments() to reuse sort)
+    this.documentListChangedEvent.next(this.getDocuments());
+  }
+
   // Deletes a document from the document list and emits the change
   deleteDocument(doc: Document) {
-    if (!doc) {
-      // If no document: leave function
+    // If no document: leave function
+    if (!doc)
       return;
-    }
 
+    // If invalid index: leave function
     const pos = this.documents.indexOf(doc);
-    if (pos < 0) {
-      // If invalid index: leave function
+    if (pos < 0)
       return;
-    }
 
     // Removing document
     this.documents.splice(pos, 1);
+
     // Emitting change (reusing getDocuments() to reuse sort)
     this.documentListChangedEvent.next(this.getDocuments());
   }
