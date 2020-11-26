ca6628feb9767da1a1d9f4e13e70e2742508e81a
naxlin <naxlin.everthaine@gmail.com>
2020-11-25 21:41:04 -0700

    done with backend

--- server/models/sequence.js
+++ server/models/sequence.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const sequenceSchema = mongoose.Schema({
-    maxDocumentId: { type: int, required: true },
-    maxMessageId: { type: int, required: true },
-    maxContactId: { type: int, required: true },
+    maxDocumentId: { type: Number, required: true },
+    maxMessageId: { type: Number, required: true },
+    maxContactId: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Sequence', sequenceSchema);
--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,3 +1,105 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const Message = require('../models/message');
+const sequenceGenerator = require('./sequenceGenerator');
+const router = express.Router();
+
+// Function to return an error if one occurs, reduces code!
+function returnError(res, err) {
+    res.status(500).json({message: "An error occurred!", error: err});
+}
+
+router.get('/', (req, res, next) => {
+    Message.find().then(mess => {
+        res.status(200).json({message: "Messages fetched successfully!", messages: mess});
+    }).catch(err => {
+        returnError(res, err);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+    Message.findOne({"id": req.params.id})
+    .then(mes => {
+        res.status(200).json({message: "Message fetched successfully!", mess: mes});
+    }).catch(err => {
+        returnError(res, err);
+    });
+});
+
+router.post('/', (req, res, next) => {
+  const maxMessageId = sequenceGenerator.nextId("Messages");
+
+  const message = new Message({
+      id: maxMessageId,
+      subject: req.body.subject,
+      msgText: req.body.msgText,
+      sender: req.body.sender,
+  });
+
+  message.save()
+    .then(createdMessage => {
+    res.status(201).json({
+        message: 'Message added successfully',
+        mess: createdMessage
+    });
+    })
+    .catch(error => {
+        res.status(500).json({
+        message: 'An error occurred',
+        error: error
+    });
+  });
+});
+
+router.put('/:id', (req, res, next) => {
+  Message.findOne({ id: req.params.id })
+    .then(message => {
+    message.name = req.body.name;
+    message.description = req.body.description;
+    message.url = req.body.url;
+
+    message.updateOne({ id: req.params.id }, message)
+        .then(result => {
+        res.status(204).json({
+            message: 'Message updated successfully'
+        })
+        })
+        .catch(error => {
+            res.status(500).json({
+            message: 'An error occurred',
+            error: error
+        });
+      });
+    })
+    .catch(error => {
+    res.status(500).json({
+        message: 'Message not found.',
+        error: { Message: 'Message not found'}
+    });
+  });
+});
+
+router.delete("/:id", (req, res, next) => {
+  Message.findOne({ id: req.params.id })
+    .then(message => {
+    message.deleteOne({ id: req.params.id })
+        .then(result => {
+        res.status(204).json({
+            message: "Message deleted successfully"
+        });
+        })
+        .catch(error => {
+            res.status(500).json({
+            message: 'An error occurred',
+            error: error
+        });
+      })
+    })
+    .catch(error => {
+    res.status(500).json({
+        message: 'Message not found.',
+        error: { message: 'Message not found'}
+    });
+  });
+});
+
 module.exports = router; 
